87576379311179edc146e843497613d465f7cd92
GustavoSuto <gustavo.suto@usp.br>
2019-11-23 10:30:08 -0300

    updated the data_prep class with the adjusted r2

--- data_prep.py
+++ data_prep.py
@@ -2,27 +2,49 @@
 """
 Created on Sun Jun 16 22:36:39 2019
 
-@author: user
+@author: Gustavo Suto 
 """
-class data_prep():
+class data_prep:
+
+    
     
     def breveDescricao(df):
+        """
+        Função breveDescricao
+        
+        Objetivo: Exclui atributos que estejam com colunas com todos os valores
+        'NaN'. Imprime na tela a quantidade de atributos/campos e a quantidade
+        de registros.
+        
+        Argumentos: um DataFrame.
+        """
+        
         df.dropna(axis=1, how="all", inplace=True)
     
         print("O data set possui: \n- {} atributos/campos; e \n- {} registros.\n".
           format(df.shape[1], df.shape[0]))
         df.info()
     
+    
+    
     def serieNulos(df, corte = 50):
         """
-         responsável: suto
+         responsável: Suto
          data: 04/05/19
-         objetivo: essa função retorna uma tupla com:
-                 (1) contendo uma pd.series com os atributos com maior proporção de nulos; e
-                 (2) uma string indicando quantos atributos estão com uma proporção de nulos acima do corte dado.
-         argumentos: são necessários 2 argumentos:
-                 (1) dataframe df - base que se deseja verificar quantos nulos têm; e
-                 (2) float corte - número entre 0 e 100 para indicar o valor mínimo da proporção de nulos. 
+        
+        objetivo: essa função retorna uma tupla com:
+                 (1) contendo uma pd.series com os atributos com maior
+                 proporção de nulos; e
+                 (2) uma string indicando quantos atributos estão com uma
+                 proporção de nulos acima do corte dado.
+                 argumentos: são necessários 2 argumentos:
+                 (1) dataframe df - base que se deseja verificar quantos nulos
+                 têm; e
+                 (2) float corte - número entre 0 e 100 para indicar o valor
+                 mínimo da proporção de nulos. 
+        
+        Argumentos: Um DataFrame e um corte em porcentagem (valor padrão é 50%).
+        
         """
         serie = (df.isnull().sum().sort_values(ascending=False)/len(df))*100
         return (serie[serie > corte], ("-> {} atributos/features/campos possuem mais de {}% de valores nulos.".
@@ -34,7 +56,7 @@ class data_prep():
         data: 27/10/19
         objetivo:   essa função retorna um dataframe com os atributos não
                     numéricos e sua respectiva cardinalidade em ordem crescente. 
-        argumentos: somente 01 (um) argumento, o DataFrame que se deseja
+        Argumentos: somente 01 (um) argumento, o DataFrame que se deseja
                     analisar.
         """
         import pandas as pd
@@ -76,3 +98,17 @@ class data_prep():
         matriz_cardialidade.sort_values(by="Cardinalidade", inplace=True, ascending=True)
         
         return matriz_cardialidade.T, df.describe()
+    
+    
+    
+    def r2_ajustado(x, y, y_pred):
+        """
+        responsável: Suto
+        data: 23/11/19
+        
+        r2_ajustado retorna o R² Ajustado e recebe como argumento as séries com
+        o valor alvo teste e o predito.
+        """
+        n = x.shape[0]
+        k = x.shape[1]
+        return (1-((n-1)/(n-(k+1)))*(1-r2_score(y, y_pred)))
